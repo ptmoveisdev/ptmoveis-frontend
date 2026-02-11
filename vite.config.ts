66bff9b21133fd6cc3157c6365021779714bafa8
Wagner dev <wagneraraujo15@gmail.com>
2026-02-10 23:42:46 -0300

    remove ssl

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [basicSsl(), inspectAttr(), react()],
+  plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
